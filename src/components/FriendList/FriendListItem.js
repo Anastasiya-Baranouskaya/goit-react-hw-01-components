a050e38f3d24d411831766f739f47b8ed92693d2
Anastasiya-Baranouskaya <nastenkabv@gmail.com>
2021-10-09 12:12:10 +0200

    styles for profile

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,10 +1,21 @@
+// import PropTypes from 'prop-types';
 import './FriendList.scss';
-export function FriendListItem({ id, isOnline, avatar, name }) {
+export function FriendListItem({ id, status, avatar, name }) {
   return (
     <li class="item" key={id}>
-      <span class="status">{isOnline}</span>
-      <img class="avatar" src={avatar} alt={name} width="48" />
+      {status ? (
+        <span className="statusOnline"></span>
+      ) : (
+        <span className="statusOffline"></span>
+      )}{' '}
+      <img class="friend-avatar" src={avatar} alt={name} width="48" />
       <p class="name">{name}</p>
     </li>
   );
 }
+// FriendListItem.propTypes = {
+//   // key: PropTypes.number.isRequired,
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   status: PropTypes.bool.isRequired,
+// };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,31 +1,31 @@
-import './Profile.scss';
+import s from './Profile.module.css';
 export function Profile({ name, tag, location, avatar, stats }) {
   return (
-    <div class="profile">
-      <div class="description">
+    <div className={s.profile}>
+      <div className={s.description}>
         <img
           src={avatar}
           alt="Аватар пользователя"
-          class="avatar"
+          className={s.avatar}
           width="335px"
         />
-        <p class="name">{name}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
+      <ul className={s.stats}>
+        <li className={s.coloms}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
+        <li className={s.coloms}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+        <li className={s.coloms}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
